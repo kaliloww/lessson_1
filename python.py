4dd340d2a38c39eedcdd1d2bb13e9d61175605c4
Kalilov Nurtilek <kalilovnurtilek1@gmail.com>
2023-11-27 18:00:02 +0600

    first commit

---
+++ python.py
@@ -0,0 +1,101 @@
+# print("hello world")
+
+# if else elif 
+
+
+# if 1 > 1:
+#     print("меня зовут Нуртилек")
+# else:
+#     print("error")
+
+# 1  ---    ==  
+# 2  > 
+
+# 
+
+# 3  -- <
+
+# print(2<3)
+# print(3<2)
+
+# 3 --    <=
+
+# print(2>=1)
+# print(2<=1)
+
+# print(2+2)
+# print(4-2)
+# print(4*4)
+# print(6/4)
+
+# print(6//4)
+
+# print(6%4)
+
+# a = int( input("введите число"))
+
+
+
+# s = a+a
+
+# print(str(s) ,"fnejwfne" )
+
+
+# print(f"{s} {a} hello world")
+# a = """femwvoenovrnovrnovfr
+# vdsldsnmivndsionvidso
+# vndkjsvnidsonvidsol
+# vmk0dslnvidsl"""
+# print(a)
+# login_1 = "pov"
+# passwor_1 = "qwerty"
+
+
+
+
+# login = input("Введите ваш логин: ")
+# passwor = input("Введите свой пароль: ")
+
+
+# if login == login_1:
+#     if passwor == passwor_1:
+#         print("добро пожаловать!!")
+#     else:
+#         print("у вас неправильный пароль введите еще раз")
+# else:
+#     print("у вас неправильный логин")
+
+#  and  , or , not
+
+# if login == login_1 and passwor==passwor_1:
+#     print("Добро пожаловать")
+# else:
+#     print("Ошибка")
+
+# if login == login_1 or passwor==passwor_1:
+#     print("Добро пожаловать")
+# else:
+#     print("Ошибка")
+
+
+# цикл for
+
+# for i in range(0,100,5):
+#     print(i)
+#     if i == 9:
+#         print("hello")
+
+
+# number = "cnwuicbwquicbjwqDKGUWQILDGUWQEILDFFGEUQWILFGUQWKDBJCBDJCBbcjkdbvudkbvjdfkvburkhvewlvnjdksvbudsivbjkvbdjskvgebkvbsdklbvsklhbvrfnbkrfnbkrfbklrnv1536456415b63f1dbfrbreb56rdb53erht64ht64b153dfb15d634b153dr1b53rb153rb15r3b153db15rt64bhht58e64hb53fb15re684b8re64gb5re6b418re6b4er86b415re64h1458e64h6454vh544h5844vy854y8654v486544"
+# n = 0
+# for i in number:
+#     if i == "5":
+#         # n=n+1
+#         n+=1
+      
+
+# print(n)
+  
+a = (1,"d",12)
+for miraj in a :
+    print(miraj)
